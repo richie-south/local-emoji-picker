c6d443ff7c395b973d6c292cfbc3437f7de2bd48
richie-south <richard.soderman@gmail.com>
2019-04-30 09:34:31 +0200

    chore: add webpack

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const webpack = require('webpack')
+const path = require('path')
+const childProcess = require('child_process')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    client: './src/index.tsx'
+  },
+  output: {
+    filename: 'emoji-list.[hash].js',
+    path: path.join(__dirname, '/dist')
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: 'source-map',
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['.ts', '.tsx', '.js', '.json']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'awesome-typescript-loader',
+            options: {
+              useCache: true
+            }
+          }
+        ]
+      },
+
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCSSExtractPlugin.loader,
+          'css-loader',
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new MiniCSSExtractPlugin({
+      filename: '[name].css'
+    }),
+  ]
+}
