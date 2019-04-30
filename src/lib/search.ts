dff8c4e8e2d49dded5e1da1d46a3157a184218dd
richie-south <richard.soderman@gmail.com>
2019-04-30 09:13:10 +0200

    fix: search

--- src/lib/search.ts
+++ src/lib/search.ts
@@ -1,28 +1,29 @@
-import { emojiList } from './emoji-list'
-import { categories } from './categories'
+import { emojiList, EmojiList } from './emoji-list'
+import { categories, Category } from './categories'
+import { EmojiData } from '../types'
 
-const matchOperatorsRegex = /[|\\{}()[\]^$+*?.-]/g;
+const matchOperatorsRegex = /[|\\{}()[\]^$+*?.-]/g
 
-const escape = string => {
-	if (typeof string !== 'string') {
-		throw new TypeError('Expected a string');
-	}
-
-	return string.replace(matchOperatorsRegex, '\\$&');
-};
-
-export function searchEmojis (term) {
-  if (!term || typeof term !== 'string') {
-    return []
+const escape = (str: string) => {
+  if (typeof str !== 'string') {
+	  throw new TypeError('Expected a string')
   }
 
-  const searchTermRegExp = new RegExp(`^(?:.* +)*${escape(term)}`, "i")
-  const keywordMatchesSearchTerm = keyword => searchTermRegExp.test(keyword)
-  const emojiMatchesSearchTerm = emoji =>
-    emoji.keywords.concat(emoji.name).some(keywordMatchesSearchTerm)
+  return str.replace(matchOperatorsRegex, '\\$&')
+}
 
-  return categories.map((category) => {
-    const categoryList = emojiList[category.category]
+export function searchEmojis (term: string): EmojiList {
+  if (!term || typeof term !== 'string') {
+    return {}
+  }
+
+  const searchTermRegExp = new RegExp(`^(?:.* +)*${escape(term)}`, 'i')
+  const keywordMatchesSearchTerm = (keywords: string) => searchTermRegExp.test(keywords)
+  const emojiMatchesSearchTerm = (emoji: EmojiData) =>
+    emoji.k.concat(emoji.n).some(keywordMatchesSearchTerm)
+
+  return categories.map((category: Category) => {
+    const categoryList: Array<EmojiData> = emojiList[category.category]
 
     const emojis = categoryList.filter(emojiMatchesSearchTerm)
 
