70905039a9bd4b849fbab5363034681d62e03438
richie-south <richard.soderman@gmail.com>
2019-05-01 13:49:49 +0200

    feat: add header of categories

---
+++ src/category-selector.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react'
+
+import { EmojiData } from './types'
+import { Category, categories } from './lib/categories'
+
+const Categories = ({ onClick }: {
+  onClick: (value: Category) => void
+}) => {
+  return (
+    <div className='categories-list'>
+      {categories.map((category) => {
+        return (
+          <div
+            key={category.category}
+            className='category'
+            onClick={() => onClick(category)}
+          >
+            {category.symbol}
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+type Props = {
+  onClick: (value: Category) => void
+}
+
+export const CategorySelector = ({
+  onClick
+}: Props) => {
+  return (
+    <div className='category-section'>
+      <Categories
+        onClick={onClick}
+      />
+    </div>
+  )
+}
