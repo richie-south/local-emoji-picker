d3726ff57c04cbc81b83d150803dd4dc0617e4da
richie-south <richard.soderman@gmail.com>
2019-05-02 08:54:07 +0200

    feat: preview multible different configs

--- preview/preview.tsx
+++ preview/preview.tsx
@@ -6,10 +6,45 @@ const onClick = (data) => {
   console.log(data)
 }
 
+// all
 ReactDOM.render(
   <LocalEmojiPicker
     categorySelector
     onClick={onClick}
+    idPrefix='example-1'
   />,
   document.getElementById('example-1')
 );
+
+// only search
+ReactDOM.render(
+  <LocalEmojiPicker
+    onClick={onClick}
+    idPrefix='example-2'
+  />,
+  document.getElementById('example-2')
+);
+
+// only category selector
+ReactDOM.render(
+  <LocalEmojiPicker
+    categorySelector
+    search={false}
+    onClick={onClick}
+    idPrefix='example-3'
+  />,
+  document.getElementById('example-3')
+);
+
+// none
+ReactDOM.render(
+  <LocalEmojiPicker
+    search={false}
+    onClick={onClick}
+    idPrefix='example-4'
+  />,
+  document.getElementById('example-4')
+);
+
+
+
