5e054ad39c471d9cc93f7e425d0b66d7e073a93f
richie-south <richard.soderman@gmail.com>
2019-05-02 14:08:46 +0200

    feat: site for publication

---
+++ webpack.site.config.js
@@ -0,0 +1,62 @@
+const webpack = require('webpack')
+const path = require('path')
+const childProcess = require('child_process')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+const FixDefaultImportPlugin = require('webpack-fix-default-import-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    app: [
+      './preview/preview.tsx',
+    ],
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    pathinfo: false,
+    filename: 'bundle.js',
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: 'source-map',
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['.ts', '.tsx', '.js', '.json']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'awesome-typescript-loader',
+            options: {
+              useCache: true
+            }
+          }
+        ]
+      },
+
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCSSExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new MiniCSSExtractPlugin({
+      filename: '[name].css'
+    }),
+    new FixDefaultImportPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'preview/index.html',
+    }),
+  ]
+}
