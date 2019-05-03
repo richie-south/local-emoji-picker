da2d59a9c848970976d93be7ef376650ff5e8485
richie-south <richard.soderman@gmail.com>
2019-05-03 08:58:35 +0200

    feat: add basic ascii to emoji converter

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
 import { Picker } from './components/picker/picker'
+import { stringToEmoji as match } from './lib/converter'
 
 export const LocalEmojiPicker = Picker
+export const stringToEmoji = match
--- src/lib/converter.ts
+++ src/lib/converter.ts
@@ -8,3 +8,36 @@ export const toFriendlyEmojiData = (data: EmojiData): FriendlyEmojiData => ({
   unicode: data.uc,
   unicodeAlt: data.uca
 })
+
+const asciiMatch = {
+  "<3": '❤️',
+  ":*": '😗',
+  ";*": '😘',
+  ":)": '🙂',
+  ":(": '🙁',
+  ":/": '😕',
+  ":|": '😐',
+  "(Y)": '👍',
+  ":D": '😃',
+  ":'(": '😢',
+  ";P": '😜',
+  ":P": '😛',
+  ":p": '😛',
+  ":o": '😮',
+  "xD": '😆',
+  "B)": '😎'
+}
+
+export const stringToEmoji = (value: string) => {
+  if (typeof value !== 'string') {
+    return value
+  }
+
+  const trimedLowerCaseValue = value.trim()
+  const hasMatch = asciiMatch[trimedLowerCaseValue]
+  if (hasMatch) {
+    return hasMatch.match
+  }
+
+  return value
+}
