95764ef9c6a01ba0a2eed89c0539e896914a7394
richie-south <richard.soderman@gmail.com>
2019-05-03 09:47:29 +0200

    feat: remove c, uc, uca and remove name from keyword

--- src/lib/converter.ts
+++ src/lib/converter.ts
@@ -3,10 +3,7 @@ import { EmojiData, FriendlyEmojiData } from '../types'
 export const toFriendlyEmojiData = (data: EmojiData): FriendlyEmojiData => ({
   value: data.v,
   name: data.n,
-  keywords: data.k,
-  category: data.c,
-  unicode: data.uc,
-  unicodeAlt: data.uca
+  keywords: data.k
 })
 
 const asciiMatch = {
--- src/types.ts
+++ src/types.ts
@@ -2,16 +2,10 @@ export type EmojiData = {
   v: string
   n: string
   k: Array<string>
-  c: string
-  uc: string
-  uca: Array<string>
 }
 
 export type FriendlyEmojiData = {
   value: EmojiData['v']
   name: EmojiData['n']
   keywords: EmojiData['k']
-  category: EmojiData['c']
-  unicode: EmojiData['uc']
-  unicodeAlt: EmojiData['uca']
 }
